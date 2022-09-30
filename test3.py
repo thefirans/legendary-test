43606cfd8b290b261d25de4ca5612b6e43a237e8
Alex Montana <59099822+thefirans@users.noreply.github.com>
2022-09-30 23:13:10 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,66 +1,7 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@thefirans 
-thefirans
-/
-Regex-validate-PIN-code
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-ReleasesTagsNew release
-New release form
- 
-Choose an existing tag, or create a new tag when you publish this release.
-
-Release title
- 
-Add heading textAdd bold text, <Ctrl+b>Add italic text, <Ctrl+i>
-Add a quote, <Ctrl+Shift+.>Add code, <Ctrl+e>Add a link, <Ctrl+k>
-Add a bulleted list, <Ctrl+Shift+8>Add a numbered list, <Ctrl+Shift+7>Add a task list, <Ctrl+Shift+l>
-Directly mention a user or team
-Reference an issue, pull request or discussion
-Add saved reply
-Describe this release
-No file chosen
-Attach files by dragging & dropping, selecting or pasting them.
-Styling with Markdown is supported
-No file chosen
- Attach binaries by dropping them here or selecting them.
-This is a pre-release
-We’ll point out that this release is identified as non-production ready.
- 
-Tagging suggestions
-It’s common practice to prefix your version names with the letter v. Some good tag names might be v1.0.0 or v2.3.4.
-
-If the tag isn’t meant for production use, add a pre-release version after the version name. Some good pre-release versions might be v0.2.0-alpha or v5.9-beta.3.
-
+S
 Semantic versioning
 If you’re new to releasing software, we highly recommend reading about semantic versioning.
 
 Footer
 © 2022 GitHub, Inc.
 Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-New release · thefirans/Regex-validate-PIN-code
