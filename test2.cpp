923d99d8201239134096ff5cf99e883e36ff5381
Alex Montana <59099822+thefirans@users.noreply.github.com>
2022-09-30 23:14:03 +0300

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,1 +1,27 @@
 grgrtkgotgjrtgjrtgrgrtg
+Skip to content
+
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settin
+ Commit directly to the main branch.
+ Create a new branch for this commit and start a pull request. Learn more about pull requests.
+ 
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
